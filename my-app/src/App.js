cf26708dbee82b0fa14a9d7c6ae379a03fc121d1
samuelawud123 <samuelawud@icloud.com>
2023-02-07 12:15:17 -0500

    Open Graph Protocol set up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,9 +4,15 @@ import Nav from './nav';
 import Main from './main';
 import Footer from './footer';
 
+
+
 function App() {
   return (
-     <div>
+    <div>
+    <meta name="Little-Lemon" content="You can order your food from for out little lemon restaurant through this cite"/>
+    <meta name="og:title" content="Little Lemon"/>
+    <meta name="og:description" content="Food ordering and reservation page for little lemon restaurant"/>
+    <meta name="og:image" content="./images/Logo.svg"/>
     <Nav/>
     <Header/>
     <Main/>
@@ -15,5 +21,5 @@ function App() {
     </div>
   );
 }
-
+ 
 export default App;
